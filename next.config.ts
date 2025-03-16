41ecebdd920acd6d17ec29ccf37cbf21614b3472
Fahad Hassan <fahadhassan921@gmail.com>
2025-03-16 23:01:46 +0400

    remove baseurl

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/testt",
+  //basePath: "/testt",
   output: "standalone",
-  trailingSlash: true,
-  assetPrefix: "/testt/",
+  // trailingSlash: true,
+  // assetPrefix: "/testt/",
 };
 
 export default nextConfig;
