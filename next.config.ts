347f26662ab418a5c86e9fb21647e4292e81fefb
Fahad Hassan <fahadhassan921@gmail.com>
2025-03-16 13:08:25 +0400

    update21

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/testt/",
+  basePath: "/testt",
   output: "standalone",
   trailingSlash: true,
   assetPrefix: "/testt/",
