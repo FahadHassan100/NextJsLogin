4c9ab8b3a1e94b3ac08706b07b6e4cb3f1abb797
Fahad Hassan <fahadhassan921@gmail.com>
2025-03-17 16:56:46 +0400

    set asset prefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // basePath: "/testt",
+  assetPrefix: '/testt',
   output: "standalone",
   // trailingSlash: true,
   // assetPrefix: "/testt/",
