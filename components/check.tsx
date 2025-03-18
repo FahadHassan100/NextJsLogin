02b4130321b0337d0c5a84249ab4f00240687f3d
Fahad Hassan <fahadhassan921@gmail.com>
2025-03-18 12:11:32 +0400

    check db value

---
+++ components/check.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { useState } from "react";
+import { getUserByEmail } from "@/services/user";
+import { useEffect } from "react";
+
+const CheckScreen = () => {
+  const [nameHai, setNameHai] = useState();
+
+  useEffect(() => {
+    const getUsr = async () => {
+      const abc: any = await getUserByEmail("test@live.com");
+      setNameHai(abc.Client_Name);
+      console.log();
+    };
+
+    getUsr();
+  }, []);
+
+  return <div>This is username from database: {nameHai && nameHai} </div>;
+};
+
+export default CheckScreen;
